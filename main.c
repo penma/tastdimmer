70bc72a19e15ef381459bb8fa0e07b894f5e76a8
Lars Stoltenow <penma@penma.de>
2013-01-30 23:27:45 +0100

    pwm auf timer0 gelegt, taster verschoben

--- main.c
+++ main.c
@@ -10,10 +10,10 @@ static int enabled = 1;
 
 static void update_light() {
 	if (enabled) {
-		TCCR1A |= (1 << COM1A1);
-		OCR1A = brightness;
+		TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
+		OCR0A = OCR0B = brightness;
 	} else {
-		TCCR1A &= ~(1 << COM1A1);
+		TCCR0A &= ~((1 << COM0A1) | (1 << COM0B1));
 	}
 }
 
@@ -24,11 +24,11 @@ static void update_light() {
 #define DIMMER_START_DELAY 100
 #define DIMMER_DIRECTION_DELAY 200
 
-ISR (TIMER0_COMPA_vect) {
+ISR (TIMER1_COMPA_vect) {
 	static uint16_t hold_step = 0;
 	static uint16_t save_step;
 
-	if (PINB & (1 << PB1)) {
+	if (PINB & (1 << PB3)) {
 		save_step = hold_step;
 		hold_step = 0;
 	} else {
@@ -84,21 +84,22 @@ ISR (TIMER0_COMPA_vect) {
 
 int main (void) {
 //	init();
-	DDRB |= (1 << PB3);
+	DDRD |= (1 << PD5);
+	DDRB |= (1 << PB2);
 
 	/* fast pwm, 9-bit, toggle OC1A, prescaler 1 */
-	TCCR1A = (1 << COM1A1) | (1 << WGM10);
-	TCCR1B = (1 << CS10) | (1 << WGM12);
-	OCR1A = 0xff;
+	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
+	TCCR0B = (1 << CS00);
+	OCR0A = OCR0B = 0xff;
 
 	/* tasterfoo */
-	DDRB &= ~(1 << PB1);
-	PORTB |= (1 << PB1);
+	DDRB &= ~(1 << PB3);
+	PORTB |= (1 << PB3);
 
-	TCCR0A = (1 << WGM01); /* CTC */
-	TCCR0B = (1 << CS02) | (1 << CS00); /* prescaler 1/1024 */
-	OCR0A = 38; /* (F_CPU/1024 * 5ms) - 1 */
-	TIMSK |= 1 << OCIE0A;
+	TCCR1A = 0;
+	TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); /* prescaler 1/1024, CTC */
+	OCR1A = 38; /* (F_CPU/1024 * 5ms) - 1 */
+	TIMSK |= 1 << OCIE1A;
 	sei();
 
 	while (1) {
